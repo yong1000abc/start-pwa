884487cb6fd019be51d17e20c07a89ed62753315
Kim Yongcheon <yong1000abc@gmail.com>
2022-07-10 00:25:14 +0900

    heroku test

--- server/server.js
+++ server/server.js
@@ -6,10 +6,9 @@ const PORT = process.env.PORT || 9000;
 
 app.use(express.static('public'));
 app.use(express.static('dist'));
-app.use('/', express.static('dist'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
